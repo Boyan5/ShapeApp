c205b0efe423129985432c3d1a05838b127f7757
Boyan5 <62429994+Boyan5@users.noreply.github.com>
2021-08-03 12:48:12 +0300

    Add files via upload

---
+++ WindowsFormsApp1/EquilateralTriangle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Project.Lib;
+
+namespace WindowsFormsApp1
+{
+    [Serializable()]
+    public partial class EquilateralTriangle : Form
+    {
+        public static double sideA;
+        public EquilateralTriangle()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDescribe_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Project.Lib.EquilateralTriangle equilateralTriangle = new Project.Lib.EquilateralTriangle(Convert.ToDouble(txtBoxA.Text));
+                lblDescribe.Text = equilateralTriangle.Describe();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter the parameters of the equilateral triangle!");
+            }
+        }
+
+        private void btnCalcArea_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Project.Lib.EquilateralTriangle equilateralTriangle = new Project.Lib.EquilateralTriangle(Convert.ToDouble(txtBoxA.Text));
+                lblCalcArea.Text = "The area of the equilateral triangle is " + equilateralTriangle.CalculateArea();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter the parameters of the equilateral triangle!");
+            }
+        }
+
+        private void btnCalcPerimeter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Project.Lib.EquilateralTriangle equilateralTriangle = new Project.Lib.EquilateralTriangle(Convert.ToDouble(txtBoxA.Text));
+                lblCalcPerimeter.Text = "The perimeter of the equilateral triangle is " + equilateralTriangle.CalculatePerimeter();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter the parameters of the equilateral triangle!");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Triangle triangle = new Triangle();
+            triangle.Show();
+        }
+
+        private void txtBoxA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBoxA.Clear();
+            lblDescribe.Text = "";
+            lblCalcArea.Text = "";
+            lblCalcPerimeter.Text = "";
+        }
+
+        private void btnDrawing_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sideA = Convert.ToDouble(txtBoxA.Text);
+                Draw draw = new Draw();
+                draw.Show();
+                this.Hide();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter the parameters of the right triangle!");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/IsoscelesTriangle.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class IsoscelesTriangle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxA = new System.Windows.Forms.TextBox();
+            this.txtBoxB = new System.Windows.Forms.TextBox();
+            this.btnDescribe = new System.Windows.Forms.Button();
+            this.btnCalcArea = new System.Windows.Forms.Button();
+            this.btnCalcPerimeter = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblDescribe = new System.Windows.Forms.Label();
+            this.lblCalcArea = new System.Windows.Forms.Label();
+            this.lblCalcPerimeter = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblSideB = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDrawing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxA
+            // 
+            this.txtBoxA.Location = new System.Drawing.Point(73, 77);
+            this.txtBoxA.Name = "txtBoxA";
+            this.txtBoxA.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxA.TabIndex = 0;
+            this.txtBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxA_KeyPress);
+            // 
+            // txtBoxB
+            // 
+            this.txtBoxB.Location = new System.Drawing.Point(73, 235);
+            this.txtBoxB.Name = "txtBoxB";
+            this.txtBoxB.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxB.TabIndex = 1;
+            this.txtBoxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxB_KeyPress);
+            // 
+            // btnDescribe
+            // 
+            this.btnDescribe.Location = new System.Drawing.Point(73, 359);
+            this.btnDescribe.Name = "btnDescribe";
+            this.btnDescribe.Size = new System.Drawing.Size(100, 43);
+            this.btnDescribe.TabIndex = 2;
+            this.btnDescribe.Text = "Describe";
+            this.btnDescribe.UseVisualStyleBackColor = true;
+            this.btnDescribe.Click += new System.EventHandler(this.btnDescribe_Click);
+            // 
+            // btnCalcArea
+            // 
+            this.btnCalcArea.Location = new System.Drawing.Point(261, 359);
+            this.btnCalcArea.Name = "btnCalcArea";
+            this.btnCalcArea.Size = new System.Drawing.Size(94, 43);
+            this.btnCalcArea.TabIndex = 3;
+            this.btnCalcArea.Text = "Calculate Area";
+            this.btnCalcArea.UseVisualStyleBackColor = true;
+            this.btnCalcArea.Click += new System.EventHandler(this.btnCalcArea_Click);
+            // 
+            // btnCalcPerimeter
+            // 
+            this.btnCalcPerimeter.Location = new System.Drawing.Point(449, 359);
+            this.btnCalcPerimeter.Name = "btnCalcPerimeter";
+            this.btnCalcPerimeter.Size = new System.Drawing.Size(108, 43);
+            this.btnCalcPerimeter.TabIndex = 4;
+            this.btnCalcPerimeter.Text = "Calculate Perimeter";
+            this.btnCalcPerimeter.UseVisualStyleBackColor = true;
+            this.btnCalcPerimeter.Click += new System.EventHandler(this.btnCalcPerimeter_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(663, 359);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 43);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblDescribe
+            // 
+            this.lblDescribe.AutoSize = true;
+            this.lblDescribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDescribe.Location = new System.Drawing.Point(239, 86);
+            this.lblDescribe.Name = "lblDescribe";
+            this.lblDescribe.Size = new System.Drawing.Size(0, 24);
+            this.lblDescribe.TabIndex = 7;
+            // 
+            // lblCalcArea
+            // 
+            this.lblCalcArea.AutoSize = true;
+            this.lblCalcArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCalcArea.Location = new System.Drawing.Point(239, 161);
+            this.lblCalcArea.Name = "lblCalcArea";
+            this.lblCalcArea.Size = new System.Drawing.Size(0, 24);
+            this.lblCalcArea.TabIndex = 8;
+            // 
+            // lblCalcPerimeter
+            // 
+            this.lblCalcPerimeter.AutoSize = true;
+            this.lblCalcPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCalcPerimeter.Location = new System.Drawing.Point(239, 237);
+            this.lblCalcPerimeter.Name = "lblCalcPerimeter";
+            this.lblCalcPerimeter.Size = new System.Drawing.Size(0, 24);
+            this.lblCalcPerimeter.TabIndex = 9;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(70, 49);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(38, 13);
+            this.lblA.TabIndex = 10;
+            this.lblA.Text = "Side A";
+            // 
+            // lblSideB
+            // 
+            this.lblSideB.AutoSize = true;
+            this.lblSideB.Location = new System.Drawing.Point(70, 203);
+            this.lblSideB.Name = "lblSideB";
+            this.lblSideB.Size = new System.Drawing.Size(38, 13);
+            this.lblSideB.TabIndex = 11;
+            this.lblSideB.Text = "Side B";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(663, 49);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 38);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDrawing
+            // 
+            this.btnDrawing.Location = new System.Drawing.Point(663, 235);
+            this.btnDrawing.Name = "btnDrawing";
+            this.btnDrawing.Size = new System.Drawing.Size(85, 39);
+            this.btnDrawing.TabIndex = 25;
+            this.btnDrawing.Text = "Draw";
+            this.btnDrawing.UseVisualStyleBackColor = true;
+            this.btnDrawing.Click += new System.EventHandler(this.btnDrawing_Click);
+            // 
+            // IsoscelesTriangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDrawing);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblSideB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblCalcPerimeter);
+            this.Controls.Add(this.lblCalcArea);
+            this.Controls.Add(this.lblDescribe);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCalcPerimeter);
+            this.Controls.Add(this.btnCalcArea);
+            this.Controls.Add(this.btnDescribe);
+            this.Controls.Add(this.txtBoxB);
+            this.Controls.Add(this.txtBoxA);
+            this.Name = "IsoscelesTriangle";
+            this.Text = "IsoscelesTriangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxA;
+        private System.Windows.Forms.TextBox txtBoxB;
+        private System.Windows.Forms.Button btnDescribe;
+        private System.Windows.Forms.Button btnCalcArea;
+        private System.Windows.Forms.Button btnCalcPerimeter;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblDescribe;
+        private System.Windows.Forms.Label lblCalcArea;
+        private System.Windows.Forms.Label lblCalcPerimeter;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblSideB;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDrawing;
+    }
+}
