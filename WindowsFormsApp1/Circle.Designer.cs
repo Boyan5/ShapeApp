b83d963733fd8133b4a98ce274f68320a5b9866c
Boyan5 <62429994+Boyan5@users.noreply.github.com>
2021-08-03 12:51:52 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Circle.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Circle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxR = new System.Windows.Forms.TextBox();
+            this.btnDescribe = new System.Windows.Forms.Button();
+            this.btnCalcArea = new System.Windows.Forms.Button();
+            this.btnCalcPerimeter = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblCalcArea = new System.Windows.Forms.Label();
+            this.lblDescribe = new System.Windows.Forms.Label();
+            this.lblCalcPerimeter = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxR
+            // 
+            this.txtBoxR.Location = new System.Drawing.Point(91, 172);
+            this.txtBoxR.Name = "txtBoxR";
+            this.txtBoxR.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxR.TabIndex = 0;
+            this.txtBoxR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxR_KeyPress);
+            // 
+            // btnDescribe
+            // 
+            this.btnDescribe.Location = new System.Drawing.Point(91, 357);
+            this.btnDescribe.Name = "btnDescribe";
+            this.btnDescribe.Size = new System.Drawing.Size(100, 44);
+            this.btnDescribe.TabIndex = 1;
+            this.btnDescribe.Text = "Describe";
+            this.btnDescribe.UseVisualStyleBackColor = true;
+            this.btnDescribe.Click += new System.EventHandler(this.btnDescribe_Click);
+            // 
+            // btnCalcArea
+            // 
+            this.btnCalcArea.Location = new System.Drawing.Point(276, 357);
+            this.btnCalcArea.Name = "btnCalcArea";
+            this.btnCalcArea.Size = new System.Drawing.Size(112, 44);
+            this.btnCalcArea.TabIndex = 2;
+            this.btnCalcArea.Text = "Calculate Area";
+            this.btnCalcArea.UseVisualStyleBackColor = true;
+            this.btnCalcArea.Click += new System.EventHandler(this.btnCalcArea_Click);
+            // 
+            // btnCalcPerimeter
+            // 
+            this.btnCalcPerimeter.Location = new System.Drawing.Point(481, 357);
+            this.btnCalcPerimeter.Name = "btnCalcPerimeter";
+            this.btnCalcPerimeter.Size = new System.Drawing.Size(115, 44);
+            this.btnCalcPerimeter.TabIndex = 3;
+            this.btnCalcPerimeter.Text = "Calculate Perimeter";
+            this.btnCalcPerimeter.UseVisualStyleBackColor = true;
+            this.btnCalcPerimeter.Click += new System.EventHandler(this.btnCalcPerimeter_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(651, 357);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 44);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(88, 144);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(40, 13);
+            this.lblR.TabIndex = 7;
+            this.lblR.Text = "Radius";
+            // 
+            // lblCalcArea
+            // 
+            this.lblCalcArea.AutoSize = true;
+            this.lblCalcArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCalcArea.Location = new System.Drawing.Point(258, 179);
+            this.lblCalcArea.Name = "lblCalcArea";
+            this.lblCalcArea.Size = new System.Drawing.Size(0, 24);
+            this.lblCalcArea.TabIndex = 8;
+            // 
+            // lblDescribe
+            // 
+            this.lblDescribe.AutoSize = true;
+            this.lblDescribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDescribe.Location = new System.Drawing.Point(258, 93);
+            this.lblDescribe.Name = "lblDescribe";
+            this.lblDescribe.Size = new System.Drawing.Size(0, 24);
+            this.lblDescribe.TabIndex = 9;
+            // 
+            // lblCalcPerimeter
+            // 
+            this.lblCalcPerimeter.AutoSize = true;
+            this.lblCalcPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCalcPerimeter.Location = new System.Drawing.Point(258, 261);
+            this.lblCalcPerimeter.Name = "lblCalcPerimeter";
+            this.lblCalcPerimeter.Size = new System.Drawing.Size(0, 24);
+            this.lblCalcPerimeter.TabIndex = 10;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(686, 245);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(92, 40);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(686, 74);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(92, 43);
+            this.btnDraw.TabIndex = 11;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Circle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.lblCalcPerimeter);
+            this.Controls.Add(this.lblDescribe);
+            this.Controls.Add(this.lblCalcArea);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCalcPerimeter);
+            this.Controls.Add(this.btnCalcArea);
+            this.Controls.Add(this.btnDescribe);
+            this.Controls.Add(this.txtBoxR);
+            this.Name = "Circle";
+            this.Text = "Circle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxR;
+        private System.Windows.Forms.Button btnDescribe;
+        private System.Windows.Forms.Button btnCalcArea;
+        private System.Windows.Forms.Button btnCalcPerimeter;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblCalcArea;
+        private System.Windows.Forms.Label lblDescribe;
+        private System.Windows.Forms.Label lblCalcPerimeter;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
---
+++ WindowsFormsApp1/Draw.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    [Serializable()]
+    public partial class Draw : Form
+    {
+        private bool btnRectWasClicked = false;
+        private bool btnSquareWasClicked = false;
+        private bool btnCircleWasClicked = false;
+        private bool btnEquiateralTriangleWasClicked = false;
+        private bool btnIsoscelesTriangleWasClicked = false;
+        private bool btnScaleneTriangleWasClicked = false;
+
+        public Draw()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        Pen p = new Pen(Color.Black);
+        Graphics g;
+
+        List<Project.Lib.IShape> shapes = new List<Project.Lib.IShape>();
+
+        Point p1;
+
+        bool getp1 = false;
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            if (Square.sideA == 0)
+            {
+                MessageBox.Show("Enter a value for the side of the square!");
+                return;
+            }
+            g.DrawRectangle(p, p1.X, p1.Y, (float)Square.sideA * 10, (float)Square.sideA * 10);
+            btnSquareWasClicked = true;
+            Project.Lib.Square square = new Project.Lib.Square(Square.sideA);
+            shapes.Add(square);
+            btnRectWasClicked = false;
+            btnCircleWasClicked = false;
+            btnEquiateralTriangleWasClicked = false;
+            btnIsoscelesTriangleWasClicked = false;
+            btnScaleneTriangleWasClicked = false;
+        }
+
+        private void btnRect_Click(object sender, EventArgs e)
+        {
+            if (Rectangle.sideA == 0 || Rectangle.sideB == 0)
+            {
+                MessageBox.Show("Enter values for the parameter of the rectangle!");
+                return;
+            }
+            g.DrawRectangle(p, p1.X, p1.Y, (float)Rectangle.sideA * 10, (float)Rectangle.sideB * 10);
+            btnRectWasClicked = true;
+            Project.Lib.Rectangle rectangle = new Project.Lib.Rectangle(Rectangle.sideA, Rectangle.sideB);
+            shapes.Add(rectangle);
+            btnSquareWasClicked = false;
+            btnCircleWasClicked = false;
+            btnEquiateralTriangleWasClicked = false;
+            btnIsoscelesTriangleWasClicked = false;
+            btnScaleneTriangleWasClicked = false;
+        }
+
+        private void DrawQuadrilateral_MouseDown(object sender, MouseEventArgs e)
+        {
+            Draw drawQuadrilateral = new Draw();
+            drawQuadrilateral.Cursor = Cursors.Cross;
+            getp1 = true;
+            if (getp1)
+            {
+                p1 = new Point(e.X, e.Y);
+            }
+            getp1 = false;
+
+            drawQuadrilateral.Cursor = Cursors.Default;
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            clrDialog.ShowDialog();
+            SolidBrush sb = new SolidBrush(clrDialog.Color);
+            if (btnRectWasClicked)
+            {
+                g.FillRectangle(sb, p1.X, p1.Y, (float)Rectangle.sideA * 10, (float)Rectangle.sideB * 10);
+                btnRectWasClicked = false;
+            }
+            if (btnSquareWasClicked)
+            {
+                g.FillRectangle(sb, p1.X, p1.Y, (float)Square.sideA * 10, (float)Square.sideA * 10);
+                btnSquareWasClicked = false;
+            }
+            if (btnCircleWasClicked)
+            {
+                g.FillEllipse(sb, p1.X, p1.Y, (float)Circle.R * 10, (float)Circle.R * 10);
+                btnCircleWasClicked = false;
+            }
+            if (btnEquiateralTriangleWasClicked)
+            {
+                var side = Convert.ToInt32(EquilateralTriangle.sideA) * 10;
+                Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + side, p1.Y + side + side / 2), new Point(p1.X - side, p1.Y + side + side / 2) };
+                g.FillPolygon(sb, points);
+                btnEquiateralTriangleWasClicked = false;
+            }
+            if (btnIsoscelesTriangleWasClicked)
+            {
+                var sideA = Convert.ToInt32(IsoscelesTriangle.sideA) * 10;
+                var sideB = Convert.ToInt32(IsoscelesTriangle.sideB) * 10;
+                Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + sideB, p1.Y + sideA), new Point(p1.X - sideB, p1.Y + sideA) };
+                g.FillPolygon(sb, points);
+                btnIsoscelesTriangleWasClicked = false;
+            }
+            if (btnScaleneTriangleWasClicked)
+            {
+                var sideA = Convert.ToInt32(ScaleneTriangle.sideA) * 10;
+                var sideB = Convert.ToInt32(ScaleneTriangle.sideB) * 10;
+                var sideC = Convert.ToInt32(ScaleneTriangle.sideC) * 10;
+                Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + sideB, p1.Y + sideA), new Point(p1.X - sideC, p1.Y + sideB) };
+                g.FillPolygon(sb, points);
+                btnScaleneTriangleWasClicked = false;
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            Shapes shapes = new Shapes();
+            shapes.Show();
+        }
+
+        private void btnLINQ_Click(object sender, EventArgs e)
+        {
+            var smallShapes = shapes.Where(c => c.CalculatePerimeter() < 20).OrderBy(p => p.CalculatePerimeter()).ThenBy(c => c.CalculateArea()).Select(c => "Perimeter of " + c.GetName() + " is " + c.CalculatePerimeter() + ".Area of " + c.GetName() + " is " + c.CalculateArea()).ToList();
+            foreach (var smallShape in smallShapes)
+            {
+                listBox1.Items.Add(smallShape);
+            }
+            var allPerimeter = shapes.Sum(c => c.CalculatePerimeter());
+            listBox1.Items.Add(Environment.NewLine + "All shapes perimeter is " + allPerimeter);
+        }
+
+        private void drawCircle_Click(object sender, EventArgs e)
+        {
+            if (Circle.R == 0)
+            {
+                MessageBox.Show("Enter a value for the radius of the circle!");
+                return;
+            }
+
+            g.DrawEllipse(p, p1.X, p1.Y, (float)Circle.R * 10, (float)Circle.R * 10);
+            btnCircleWasClicked = true;
+            Project.Lib.Circle circle = new Project.Lib.Circle(Circle.R);
+            shapes.Add(circle);
+            btnRectWasClicked = false;
+            btnSquareWasClicked = false;
+            btnEquiateralTriangleWasClicked = false;
+            btnIsoscelesTriangleWasClicked = false;
+            btnScaleneTriangleWasClicked = false;
+        }
+
+        private void btnTriag_Click(object sender, EventArgs e)
+        {
+            if (EquilateralTriangle.sideA == 0)
+            {
+                MessageBox.Show("Enter a value for the sides of the equilateral triangle!");
+                return;
+            }
+
+            var side = Convert.ToInt32(EquilateralTriangle.sideA) * 10;
+            Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + side, p1.Y + side + side / 2), new Point(p1.X - side, p1.Y + side + side / 2) };
+            g.DrawPolygon(p, points);
+            btnEquiateralTriangleWasClicked = true;
+            Project.Lib.EquilateralTriangle equilateralTriangle = new Project.Lib.EquilateralTriangle(EquilateralTriangle.sideA);
+            shapes.Add(equilateralTriangle);
+            btnRectWasClicked = false;
+            btnSquareWasClicked = false;
+            btnCircleWasClicked = false;
+            btnIsoscelesTriangleWasClicked = false;
+            btnScaleneTriangleWasClicked = false;
+        }
+
+        private void btnDrawIsoscelesTriag_Click(object sender, EventArgs e)
+        {
+            if (IsoscelesTriangle.sideA == 0 || IsoscelesTriangle.sideB == 0)
+            {
+                MessageBox.Show("Enter a value for the sides of the isosceles triangle!");
+                return;
+            }
+
+            var sideA = Convert.ToInt32(IsoscelesTriangle.sideA) * 10;
+            var sideB = Convert.ToInt32(IsoscelesTriangle.sideB) * 10;
+            Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + sideB, p1.Y + sideA), new Point(p1.X - sideB, p1.Y + sideA) };
+            g.DrawPolygon(p, points);
+            btnIsoscelesTriangleWasClicked = true;
+            Project.Lib.IsoscelesTriangle isoscelesTriangle = new Project.Lib.IsoscelesTriangle(IsoscelesTriangle.sideA, IsoscelesTriangle.sideB);
+            shapes.Add(isoscelesTriangle);
+            btnRectWasClicked = false;
+            btnSquareWasClicked = false;
+            btnCircleWasClicked = false;
+            btnEquiateralTriangleWasClicked = false;
+            btnScaleneTriangleWasClicked = false;
+        }
+
+        private void btnDrawScaleneTriag_Click(object sender, EventArgs e)
+        {
+            if (ScaleneTriangle.sideA == 0 || ScaleneTriangle.sideB == 0 || ScaleneTriangle.sideC == 0)
+            {
+                MessageBox.Show("Enter a value for the sides of the scalene triangle!");
+                return;
+            }
+
+            var sideA = Convert.ToInt32(ScaleneTriangle.sideA) * 10;
+            var sideB = Convert.ToInt32(ScaleneTriangle.sideB) * 10;
+            var sideC = Convert.ToInt32(ScaleneTriangle.sideC) * 10;
+            Point[] points = new Point[] { new Point(p1.X, p1.Y), new Point(p1.X + sideB, p1.Y + sideA), new Point(p1.X - sideC, p1.Y + sideB) };
+            g.DrawPolygon(p, points);
+            btnScaleneTriangleWasClicked = true;
+            Project.Lib.ScaleneTriangle scaleneTriangle = new Project.Lib.ScaleneTriangle(ScaleneTriangle.sideA, ScaleneTriangle.sideB, ScaleneTriangle.sideC);
+            shapes.Add(scaleneTriangle);
+            btnRectWasClicked = false;
+            btnSquareWasClicked = false;
+            btnCircleWasClicked = false;
+            btnEquiateralTriangleWasClicked = false;
+            btnIsoscelesTriangleWasClicked = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                IFormatter formatter = new BinaryFormatter();
+                using (Stream stream = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write))
+                {
+                    formatter.Serialize(stream, shapes);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                IFormatter formatter = new BinaryFormatter();
+                using (Stream stream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read))
+                {
+                    shapes = (List<Project.Lib.IShape>)formatter.Deserialize(stream);
+                }
+            }
+        }
+    }
+}
